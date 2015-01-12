df7e4e72074e72a97660f4ed2003de3db21fdb8b
Aljamis <alfonsov7@comcast.net>
2015-01-11 21:09:18 -0600

    Another comment

--- src/org/avr/fileread/records/IRecord.java
+++ src/org/avr/fileread/records/IRecord.java
@@ -2,6 +2,12 @@ package org.avr.fileread.records;
 
 import java.util.List;
 
+
+/**
+ * Common interface for Records (Header, Trailer, data records and Mega Fields
+ * @author Alfonso
+ *
+ */
 public interface IRecord {
 
 	public List<Field> getFields();
