86dda4fdb6f24ee5a521c99f1c3c4c8488b44336
AlfonsoPC <alfonsov7@comcast.net>
2016-08-08 13:12:06 -0500

    changes from the PC

--- src/org/avr/fileread/records/UnParsableRecord.java
+++ src/org/avr/fileread/records/UnParsableRecord.java
@@ -10,12 +10,17 @@ package org.avr.fileread.records;
  */
 public class UnParsableRecord {
 	
-	public UnParsableRecord(String line) {
+	public UnParsableRecord(String desc , String line) {
+		this.description = desc;
 		this.line = line;
 	}
 	
 	private String line;
 	public String getLine() { return line; }
 	public void setLine(String ln) { this.line = ln; }
+	
+	private String description;
+	public String getDescription() { return description; }
+	public void setDescription(String description) { this.description = description; }
 
 }
--- testing/test/avr/ParseFile.java
+++ testing/test/avr/ParseFile.java
@@ -1,6 +1,8 @@
 package test.avr;
 
 import org.avr.Member;
+import org.avr.MyHeader;
+import org.avr.MyTrailer;
 import org.avr.fileread.FileReadWriter;
 import org.avr.fileread.records.UnParsableRecord;
 
@@ -17,17 +19,20 @@ public class ParseFile {
 		try{ 
 			FileReadWriter reader = new FileReadWriter(args[0] , args[1]);
 			
-			System.out.println( reader.displayFields() );
+			//System.out.println( reader.displayFields() );
 			
 			Object obj = null;
 			while ( (obj = reader.readNextLine()) != null ) {
 				if (obj instanceof Member) {
 					Member mbr = (Member) obj;
-					System.out.println( mbr.getEmail() );
-				} else {
-					if ( obj instanceof UnParsableRecord)
-						System.out.println("Unparseable");
-				}
+					System.out.println( mbr.getFirstName() +" "+ mbr.getLastName() );
+				} else if( obj instanceof MyHeader ) {
+					System.out.println("  Rec Count : "+ ((MyHeader)obj).getRecCount() );
+				} else if ( obj instanceof UnParsableRecord) {
+					System.out.println( ((UnParsableRecord)obj).getDescription() +" : "+ ((UnParsableRecord)obj).getLine() );
+				} else if( obj instanceof MyTrailer ) {
+					System.out.println(" TRLR Count : "+ ((MyTrailer)obj).getRecCount() );
+				} 
 				
 			}
 		} catch (Exception ex) {
